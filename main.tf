7caaa93b49b2ea7cc83c0e95e0a6150195b3fdea
Allan Marcel Brunes <46986006+allanmbrunes@users.noreply.github.com>
2021-05-16 09:54:14 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -189,3 +189,63 @@ resource "oci_core_instance" "coserver" {
     ssh_authorized_keys = var.ssh_public_key
   }
 }
+
+
+resource "oci_load_balancer" "lb1" { 
+  shape = "100Mbps" 
+  compartment_id = var.compartment_ocid
+
+  subnet_ids = [
+    oci_core_subnet.financial_subnet.id,
+  ]
+  is_private = false 
+  display_name = "lb1" 
+}
+
+resource "oci_load_balancer_backend_set" "lb-bes1" { 
+  name = "lb-bes1" 
+  load_balancer_id = oci_load_balancer.lb1.id
+  policy = "WEIGHTED_ROUND_ROBIN"
+    health_checker { 
+      port = "80" 
+      protocol = "HTTP" 
+      response_body_regex = ".*" 
+      url_path = "/" 
+    } 
+}  
+
+resource "oci_load_balancer_listener" "lb-listener1" { 
+  load_balancer_id = oci_load_balancer.lb1.id
+  name = "http" 
+  default_backend_set_name = oci_load_balancer_backend_set.lb-bes1.name 
+  port = 80 
+  protocol = "HTTP" 
+  
+  
+  connection_configuration { 
+    idle_timeout_in_seconds = "10" 
+  } 
+}
+
+resource "oci_load_balancer_backend" "lb-be1" {
+  load_balancer_id = oci_load_balancer.lb1.id
+  backendset_name  = oci_load_balancer_backend_set.lb-bes1.name
+  ip_address       = oci_core_instance.fiserver.private_ip
+  port             = 80
+  backup           = false
+  drain            = false
+  offline          = false
+  weight           = 1
+}
+
+
+resource "oci_load_balancer_backend" "lb-be2" {
+  load_balancer_id = oci_load_balancer.lb1.id
+  backendset_name  = oci_load_balancer_backend_set.lb-bes1.name
+  ip_address       = oci_core_instance.fiserver.private_ip
+  port             = 80
+  backup           = false
+  drain            = false
+  offline          = false
+  weight           = 1
+}
